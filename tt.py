caa98d0fa261fcc0225a1d332bc8563c90ce7d47
minseo0619 <153469756+minseo0619@users.noreply.github.com>
2023-12-12 10:35:57 +0900

    Add files via upload

---
+++ tt.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import streamlit as st
+
+fig, ax = plt.subplots()
+
+c1 = st.radio('선의 색을 선택하시오', ['red', 'green', 'blue', 'orange'])
+s1 = st.radio('선의 색을 선택하시오', ['-', '--', ':', '-.'])
+m1 = st.radio('마커의 형태를 선택하시오', ['o', '^', 's', 'p'])
+
+x = []
+y1 = []
+y2 = []
+
+a = st.number_input('a의 값을 입력하시오', value=2.0, step=1.0)
+b = st.number_input('b의 값을 입력하시오', value=-1.0, step=1.0)
+c = st.number_input('c의 값을 입력하시오', value=15.0, step=1.0)
+d = st.number_input('d의 값을 입력하시오', value=2000.0, step=100.0)
+
+for i in range (-29, 30, 3):
+    x.append(i)
+    y1.append(a*i*i + b*i + c)
+    y2.append(d/i)
+
+plt.plot(x, y1, y2, color=c1, linestyle=s1, marker=m1)
+
+st.pyplot(fig)
